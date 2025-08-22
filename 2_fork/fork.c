62715c84d5dec601c2ae009b89787b5861a37c04
Tpmonkey-Nuttee <nuttee.2548@hotmail.com>
2025-08-22 11:10:58 +0700

    Improve code quality

--- 2_fork/fork.c
+++ 2_fork/fork.c
@@ -7,24 +7,21 @@
 
 int main() {
 	pid_t pids[10];
-	pid_t currentPid = getpid();
 
 	for (size_t i = 0; i < 10; i++) {
 		if ((pids[i] = fork()) < 0) {
 			printf("Fork failed\n");
 			return 1;
 		} else if (pids[i] == 0) {
-			currentPid = getpid();
-			printf("I'm the child numer %zu (pid %d)\n", i+1, currentPid);
+			printf("I'm the child numer %zu (pid %d)\n", i, getpid());
 			exit(0);
 		}
 	}
 
 	for (size_t i = 0; i < 10; i++) {
-		int status;
-		wait(&status); // magic fuck 2
+		wait(NULL);
 	}
-	printf("Parent terminated (pid %d)\n", currentPid);
+	printf("Parent terminated (pid %d)\n", getpid());
 
 	return 0;
 }
