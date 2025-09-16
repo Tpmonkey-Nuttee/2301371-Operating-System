71443567da9650c77a4dd60c75392ecdba1d261f
Tpmonkey-Nuttee <nuttee.2548@hotmail.com>
2025-09-16 11:55:39 +0700

    Fix connection reset
    Signed-off-by: Tpmonkey-Nuttee <nuttee.2202@gmail.com>

--- 3_webserver/server.c
+++ 3_webserver/server.c
@@ -4,15 +4,36 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <threads.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define NUM_THREADS 8
+#define MAX_HDR_SIZE 512
 
 
-void send_ok_header(int client) {
-	const char *hdr = "HTTP/1.1 200 OK\r\n"
-			  "Content-Type: text/html\r\n"
-			  "\r\n";
+int get_file_size(const char *path) {
+	// TODO: don't open file twice. but fuck this for now
+	int size;
+	FILE *f = fopen(path, "r");
+	fseek(f, 0, SEEK_END);
+	size = ftell(f);
+	fclose(f);
+	return size;
+}
+
+void send_ok_header(int client, int size) {
+	char hdr[MAX_HDR_SIZE];
+	sprintf(
+			hdr,
+		       	"HTTP/1.1 200 OK\r\n"
+			"Content-Type: text/html\r\n"
+			"Connection: close\r\n"
+			"Content-Length: %d\r\n"
+			"\r\n",
+			size
+	);
+        
 	send(client, hdr, strlen(hdr), 0);
 }
 
@@ -33,7 +54,19 @@ void send_file(int client, const char *path) {
 }
 
 
-int main() {
+void accept_client_thread(int* client) {
+	printf("Accept client %d\n", *client);
+	
+	int size = get_file_size("index.html");
+	send_ok_header(*client, size);	
+	send_file(*client, "index.html");
+	
+	close(*client);
+	printf("Client closed %d\n", *client);
+}
+
+
+int create_socket() {
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
 		perror("socket");
@@ -62,25 +95,28 @@ int main() {
 		exit(1);
 	}
 
-	printf("Listening on http://localhost:%d\n", PORT);
+	return server_fd;
+}
 	
-	for (;;) {
-		struct sockaddr_in cli;
-		socklen_t len = sizeof cli;
+int main() {
+	int server_fd = create_socket();
 
+	// Thread
+	pthread_t threads[NUM_THREADS];
+	// TODO: THREADING AAAAA
+
+	printf("Listening on http://localhost:%d\n", PORT);
+	struct sockaddr_in cli;
+	socklen_t len = sizeof cli;
+
+	for (;;) {
 		int client = accept(server_fd, (struct sockaddr*)&cli, &len);
 		if (client < 0) {
 			perror("accept");
 			continue;
 		}
-		
-		printf("Client connected %d\n", client);
-		send_ok_header(client);
-		send_file(client, "index.html");
-		printf("Client disconnected %d\n", client);
+		accept_client_thread(&client);
 
-
-		close(client);
 	}
 	
 	close(server_fd);
