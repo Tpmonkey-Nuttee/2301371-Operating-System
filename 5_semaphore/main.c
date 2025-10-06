21bf2f817e858ced711b76e8ae672933520f08de
Tpmonkey-Nuttee <nuttee.2548@hotmail.com>
2025-10-06 18:31:49 +0700

    hw5: first attempt

---
+++ 5_semaphore/main.c
@@ -0,0 +1,78 @@
+// WARNING: I don't even know what the teacher want us to code...
+// this is me guessing what he wants us to do...
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+int x = 0;
+
+sem_t w_mutex;
+
+
+
+void wait_a_sec() {
+	time_t start, now;
+	time(&start);
+	time(&now);
+	
+	// what in the fuck did i cook
+	for (;now < start + 1; time(&now)) {}
+}
+
+
+void *writer() {
+	sem_wait(&w_mutex);
+
+	wait_a_sec();
+
+	x++;
+	printf("Write x = %i\n", x);
+	
+	sem_post(&w_mutex);
+}
+
+
+void *reader() {
+	wait_a_sec();
+
+	sem_wait(&w_mutex); // ?????
+	
+	printf("Read  x = %i\n", x);
+	
+	sem_post(&w_mutex);
+}
+
+
+int main() {
+	pthread_t threads[1000];
+	time_t start, end;
+	
+	sem_init(&w_mutex, 0, 1);
+	
+	time(&start);
+	
+	for (int i = 0; i < 1000; i++) {
+		if (i % 100 == 0) { // ah yes, "randomize" a writer lol
+			pthread_create(&threads[i], NULL, writer, NULL);
+		}
+		else {
+			pthread_create(&threads[i], NULL, reader, NULL);
+		}
+	}
+
+
+	for (int i = 0; i < 1000; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	time(&end);
+
+	printf("Finished in %li seconds.\n", end-start);
+
+	sem_destroy(&w_mutex);
+	return 0;
+}
