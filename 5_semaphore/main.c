9961db7ba0247536e512cbe1e59dfa649600ef97
Tpmonkey-Nuttee <nuttee.2202@gmail.com>
2025-10-09 09:40:51 +0700

    hw5: done

--- 5_semaphore/main.c
+++ 5_semaphore/main.c
@@ -7,13 +7,49 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+#define READER_COUNT 990
+#define WRITER_COUNT 10
+#define TOTAL_THREADS READER_COUNT + WRITER_COUNT
 
 int x = 0;
+
 sem_t w_mutex;
 sem_t r_mutex;
 sem_t mutex;
 int read_count = 0;
+
+bool in_array(int *arr, int num, int size) {
+	for (size_t i = 0; i < size; i++) {
+		if (arr[i] == num) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int compare( const void* a, const void* b)
+{
+     int int_a = * ( (int*) a );
+     int int_b = * ( (int*) b );
+
+     if ( int_a == int_b ) return 0;
+     else if ( int_a < int_b ) return -1;
+     else return 1;
+}
+
+void sample(int *arr, int count, int max) {
+	int random;
+	srand(time(NULL));
+	for (int i = 0; i < count; i++) {
+		do {
+			random = rand() % max;
+		} while (in_array(arr, random, count));
+		arr[i] = random;
+	}
+	qsort(arr, count, sizeof(int), compare);
+}
 
 void wait_a_sec() {
 	time_t start, now;
@@ -25,61 +61,77 @@ void wait_a_sec() {
 }
 
 
-void *writer() {
+void *writer(void *arg) {
+	int self = *((int *) arg);
 	sem_wait(&w_mutex);
+	sem_wait(&r_mutex);
+
 	wait_a_sec();
 	x++;
-	printf("Write x = %i\n", x);
+	printf("no = %-3i x = %i\n", self, x);
+
 	sem_post(&w_mutex);
+	sem_post(&r_mutex);
 }
 
 
 void *reader() {
-	sem_wait(&r_mutex);
-	++read_count;
-	if (read_count == 1) {sem_wait(&w_mutex);}
-	sem_post(&r_mutex);
+	sem_wait(&w_mutex);
+	sem_post(&w_mutex);
 
-	
-	// This is a critical section
+	sem_wait(&mutex);
+	++read_count;
+	if (read_count == 1) {sem_wait(&r_mutex);}
+	sem_post(&mutex);
+
 	wait_a_sec();
-	printf("Read  x = %i\n", x);
-	// -----------
+//	printf("Read  x = %i\n", x);
 	
-	sem_wait(&r_mutex);
+	sem_wait(&mutex);
 	--read_count;
-	if (read_count == 0) {sem_post(&w_mutex);}
-	sem_post(&r_mutex);
+	if (read_count == 0) {sem_post(&r_mutex);}
+	sem_post(&mutex);
+	
 }
 
+
 int main() {
-	pthread_t threads[1000];
-	time_t start, end;
+	pthread_t threads[TOTAL_THREADS];
+	time_t start_t, end_t;
+	int *ptr;
 	
 	sem_init(&w_mutex, 0, 1);
 	sem_init(&r_mutex, 0, 1);
-	sem_init(&mutex, 0, 99);
+	sem_init(&mutex, 0, 1);
 
-	time(&start);
-	
-	for (int i = 0; i < 1000; i++) {
-		if (i % 100 == 0) { // ah yes, "randomize" a writer lol
-			pthread_create(&threads[i], NULL, writer, NULL);
+	// random writers
+	int random_n[WRITER_COUNT];
+	sample(random_n, WRITER_COUNT, TOTAL_THREADS);
+	int curr = 0;
+
+	time(&start_t);	
+
+	for (int i = 0; i < TOTAL_THREADS; i++) {
+		if (i == random_n[curr]) {
+			pthread_create(&threads[i], NULL, writer, &random_n[curr]);
+			curr++;
 		}
 		else {
 			pthread_create(&threads[i], NULL, reader, NULL);
 		}
 	}
-
-
-	for (int i = 0; i < 1000; i++) {
+	
+	
+	for (int i = 0; i < TOTAL_THREADS; i++) {
 		pthread_join(threads[i], NULL);
 	}
 
-	time(&end);
+	time(&end_t);
 
-	printf("Finished in %li seconds.\n", end-start);
+	printf("Finished in %li seconds.\n", end_t - start_t);
 
 	sem_destroy(&w_mutex);
+	sem_destroy(&r_mutex);
+	sem_destroy(&mutex);
 	return 0;
 }
