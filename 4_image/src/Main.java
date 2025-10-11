c15610fd25c8870b91839e519f38a51894d13b8b
Smith8bit <thesmithdot@gmail.com>
2025-10-11 13:44:35 +0700

    finalize Main.java

--- 4_image/src/Main.java
+++ 4_image/src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         String outputName = args[1];
         System.out.println("Input file: " + fileName);
 
+        // Open input image
         BufferedImage image = null;
         try {
             image = ImageIO.read(new File(fileName));
@@ -31,38 +32,39 @@ public class Main {
         int[][] inputImage = new int[height][width];
         int[][] outputArray = new int[height][width];
 
+        // Use getSample() instead of getRGB() to match PIL in Python
         Raster raster = image.getData();
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 inputImage[y][x] = raster.getSample(x, y, 0) & 0xFF;
             }
         }
-        
+
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int oldPixel = inputImage[y][x];
                 int newPixel = (oldPixel > 128) ? 255 : 0;
                 outputArray[y][x] = newPixel;
-                int quantError = oldPixel - newPixel;
+                int error = oldPixel - newPixel;
 
-                // Distribute the quantization error to neighboring pixels
-                // Use Math.floorDiv to match Python's // operator
+                // Use Math.floorDiv() to match // in Python
                 if (x + 1 < width) {
-                    inputImage[y][x + 1] += Math.floorDiv(quantError * 7, 16);
+                    inputImage[y][x + 1] += Math.floorDiv(error * 7, 16);
                 }
                 if (x - 1 >= 0 && y + 1 < height) {
-                    inputImage[y + 1][x - 1] += Math.floorDiv(quantError * 3, 16);
+                    inputImage[y + 1][x - 1] += Math.floorDiv(error * 3, 16);
                 }
                 if (y + 1 < height) {
-                    inputImage[y + 1][x] += Math.floorDiv(quantError * 5, 16);
+                    inputImage[y + 1][x] += Math.floorDiv(error * 5, 16);
                 }
                 if (x + 1 < width && y + 1 < height) {
-                    inputImage[y + 1][x + 1] += Math.floorDiv(quantError, 16);
+                    inputImage[y + 1][x + 1] += Math.floorDiv(error, 16);
                 }
             }
         }
 
-        // Save the dithered image
+        // Save processed image
         BufferedImage outImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
@@ -72,6 +74,7 @@ public class Main {
             }
         }
 
+        // Write image out
         try {
             ImageIO.write(outImg, "png", new File(outputName));
         } catch (IOException e) {
