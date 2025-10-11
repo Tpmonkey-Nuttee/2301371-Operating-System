0836806fe1b3b42004090d56990f851e2e8b9409
Smith8bit <thesmithdot@gmail.com>
2025-10-11 13:25:33 +0700

    100% Similarity w/AI

--- 4_image/src/Main.java
+++ 4_image/src/Main.java
@@ -2,13 +2,13 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import java.awt.image.Raster;
 import javax.imageio.ImageIO;
 
 public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("java src/Main.java <input> <output>");
+            System.out.println("Usage: java Main <input> <output>");
             return;
         }
 
@@ -27,37 +27,51 @@ public class Main {
         int width = image.getWidth();
         int height = image.getHeight();
 
-        int[][] input = new int[height + 2][width + 2];
-        byte[][] output = new byte[height][width];
+        // Use int array to match Python's int32
+        int[][] imgArray = new int[height][width];
+        int[][] ditheredImg = new int[height][width];
 
-
-        for (int m = 0; m < height; m++) {
-            for (int n = 0; n < width; n++) {
-                int gray = image.getRGB(n, m) & 0xFF;
-                input[m + 1][n + 1] = gray;
+        Raster raster = image.getData();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                imgArray[y][x] = raster.getSample(x, y, 0) & 0xFF;
             }
         }
 
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= width; j++) {
-                int oldVal = input[i][j];
-                int newVal = (oldVal < 128) ? 0 : 255;
-                output[i - 1][j - 1] = (byte) (newVal == 0 ? 0 : 1);
+        // Sequential error diffusion
+        int threshold = 128;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int oldPixel = imgArray[y][x];
+                int newPixel = (oldPixel > threshold) ? 255 : 0;
+                ditheredImg[y][x] = newPixel;
+                int quantError = oldPixel - newPixel;
 
-                int err = oldVal - newVal;
-
-                input[i][j + 1] += (err * 7) / 16;
-                input[i + 1][j - 1] += (err * 3) / 16;
-                input[i + 1][j] += (err * 5) / 16;
-                input[i + 1][j + 1] += err / 16;
+                // Distribute the quantization error to neighboring pixels
+                // Use Math.floorDiv to match Python's // operator
+                if (x + 1 < width) {
+                    imgArray[y][x + 1] += Math.floorDiv(quantError * 7, 16);
+                }
+                if (x - 1 >= 0 && y + 1 < height) {
+                    imgArray[y + 1][x - 1] += Math.floorDiv(quantError * 3, 16);
+                }
+                if (y + 1 < height) {
+                    imgArray[y + 1][x] += Math.floorDiv(quantError * 5, 16);
+                }
+                if (x + 1 < width && y + 1 < height) {
+                    imgArray[y + 1][x + 1] += Math.floorDiv(quantError * 1, 16);
+                }
             }
         }
 
+        // Save the dithered image
         BufferedImage outImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                int val = (output[y][x] == 0) ? 0x000000 : 0xFFFFFF;
-                outImg.setRGB(x, y, val);
+                // Clip values like Python does with np.clip(dithered_img, 0, 255)
+                int val = Math.max(0, Math.min(255, ditheredImg[y][x]));
+                int rgb = (val == 0) ? 0x000000 : 0xFFFFFF;
+                outImg.setRGB(x, y, rgb);
             }
         }
 
